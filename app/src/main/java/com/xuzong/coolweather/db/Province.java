f468fd41a0a3e947765a1dac7f8366a4c950506e
XZ <1741071488@qq.com>
2019-01-06 09:39:28 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/xuzong/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.xuzong.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by xuzong on 2019/1/6.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+}
